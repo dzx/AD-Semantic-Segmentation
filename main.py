beb13c7dc2b2a9e440e51352b9e95788128e014b
sasha <dzx303@gmail.com>
2018-02-03 12:40:59 -0800

    Some experimentation

--- main.py
+++ main.py
@@ -32,10 +32,19 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    for n in graph.collections:
+        print(n, graph.get_collection_ref(n))
 
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
+tests.test_load_vgg(load_vgg, tf)
+#%%
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
@@ -47,6 +56,8 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', 
+                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     return None
 tests.test_layers(layers)
 
@@ -64,7 +75,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     return None, None, None
 tests.test_optimize(optimize)
 
-
+#%%
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
     """
@@ -84,7 +95,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     pass
 tests.test_train_nn(train_nn)
 
-
+#%%
 def run():
     num_classes = 2
     image_shape = (160, 576)
@@ -102,6 +113,8 @@ def run():
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
+        image_input, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        print(image_input, keep_prob, layer3_out, layer4_out, layer7_out)
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
@@ -120,3 +133,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
